d233c7f1ce904b9d2785dc1319e0d3dc3901df74
JafarbekH <zokirovjafar2004@gmail.com>
2024-07-13 20:00:04 +0500

    4-dars done

--- jsscript.js
+++ jsscript.js
@@ -1,18 +1,68 @@
-// 3-dars O'ZGARUVCHILAR
-// console.log(age);
-let age = 22; //o'zgaruvchi o'zgaruvchilar 😃
-const name = "Ja'far"; //o'zgarmas o'zgaruvchilar😁
+// // 3-dars O'ZGARUVCHILAR
+// // console.log(age);
+// let age = 22; //o'zgaruvchi o'zgaruvchilar 😃
+// const name = "Ja'far"; //o'zgarmas o'zgaruvchilar😁
 
-const person = {
-    name: "Ja'far"
-}
+// const person = {
+//     name: "Ja'far"
+// }
 
-person.name = "Dovud"
+// person.name = "Dovud"
 
-// console.log(person);
-console.log(fullName);
-// old syntaxis😂
+// // console.log(person);
+// console.log(fullName);
+// // old syntaxis😂
 
-var fullName = "Ja'far Zokirov";
+// var fullName = "Ja'far Zokirov";
+
+
+
+// 4-dars MA'LUMOT TURLARI
+
+// #1 Number 
+// #2 Sonlar 
+// #3 Boolean 
+// #4 Null 
+// #5 Undifined 
+
+// const age = 24 
+// console.log(typeof age);
+
+// const name = "Ja'far"
+// console.log(typeof name);
+
+// const mantiqiy = 3 < 2
+// console.log(typeof mantiqiy);
+
+// console.log(typeof kanka); kanka nomli o'zgaruvchi bo'lmaganligi sababli  uning type undefined chiqadi
+
+// let abay 
+// console.log(abay);
+
+
+// OBJECT KOMPLEKS MA'LUMOT TURLARI
+
+// #1 Array -Massiv
+// #2 Function
+// #3 Object
+// #4 Muntazam iboralar
+// #5 Error
+
+
+// const person = {
+//     name: "Ja'far",
+//     surname: "Zokirov",
+//     fullname: "Ja'far Hasan o'g'li"
+// }
+
+// console.log(typeof person);
+
+// const cars = ["bmw", "mers"]
+// console.log(typeof cars);
+
+
+
+
+
 
 
