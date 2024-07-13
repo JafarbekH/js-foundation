84a3fdb631aaf59c1dad95a4799032578f7e0842
JafarbekH <zokirovjafar2004@gmail.com>
2024-07-13 17:22:14 +0500

    3-dars

---
+++ jsscript.js
@@ -0,0 +1,18 @@
+// 3-dars O'ZGARUVCHILAR
+// console.log(age);
+let age = 22; //o'zgaruvchi o'zgaruvchilar 😃
+const name = "Ja'far"; //o'zgarmas o'zgaruvchilar😁
+
+const person = {
+    name: "Ja'far"
+}
+
+person.name = "Dovud"
+
+// console.log(person);
+console.log(fullName);
+// old syntaxis😂
+
+var fullName = "Ja'far Zokirov";
+
+
