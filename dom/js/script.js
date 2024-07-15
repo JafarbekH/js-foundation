8753555017db2c708fd3ca99f1099aeaae4e0315
JafarbekH <zokirovjafar2004@gmail.com>
2024-07-15 14:27:44 +0500

    done

---
+++ dom/js/script.js
@@ -0,0 +1,9 @@
+//Eski usul 
+
+// const box = document.getElementById('box')
+// const box = document.getElementsByClassName('box')
+// const buttons = document.getElementsByTagName('button')
+
+// Yangi usul 
+// const box = document.querySelectorAll('button')
+// console.log(box);
