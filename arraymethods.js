b93fcfafaf8d9f307771eb9348a538fff76859fc
JafarbekH <zokirovjafar2004@gmail.com>
2024-07-17 17:43:13 +0500

    array method

---
+++ arraymethods.js
@@ -0,0 +1,41 @@
+// MAP 
+// const arr = ["BMW", "MERS", "NEXIA", "SUZUKI"]
+// const result = arr.map(item => item.toLowerCase())
+// console.log(result);
+
+
+// FILTER 
+// const arr = [
+//     {
+//         name: 'BMW',
+//         model: '2022'
+//     },
+//     {
+//         name: 'MERC',
+//         model: '2020'
+//     },
+//     {
+//         name: 'MATIZ',
+//         model: '2002'
+//     },
+//     {
+//         name: 'nexia',
+//         model: '2015'
+//     },
+// ]
+
+// const result = arr.filter(item => {
+//     return item.model > 2010
+// })
+
+// console.log(result);
+
+// REDUCE 
+// const arr = [3, 4, 2, 1, 4, 2]
+// const result = arr.reduce((prev, current) => {
+//     return prev + current
+// })
+
+// console.log(result);
+
+
